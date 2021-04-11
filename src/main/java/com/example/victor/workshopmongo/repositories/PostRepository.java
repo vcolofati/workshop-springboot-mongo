269fb2af8f780acce386d1c79a3764f9caf12c10
Victor <vcolofati@gmail.com>
2021-04-11 15:10:35 -0300

    Query methods search

--- src/main/java/com/example/victor/workshopmongo/repositories/PostRepository.java
+++ src/main/java/com/example/victor/workshopmongo/repositories/PostRepository.java
@@ -1,9 +1,12 @@
 package com.example.victor.workshopmongo.repositories;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.example.victor.workshopmongo.domain.Post;
 
 public interface PostRepository extends MongoRepository<Post, String> {
 	
+	List<Post> findByTitleContainingIgnoreCase(String text);
 }
--- src/main/java/com/example/victor/workshopmongo/resources/PostResource.java
+++ src/main/java/com/example/victor/workshopmongo/resources/PostResource.java
@@ -1,12 +1,17 @@
 package com.example.victor.workshopmongo.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.victor.workshopmongo.domain.Post;
+import com.example.victor.workshopmongo.resources.util.URL;
 import com.example.victor.workshopmongo.services.PostService;
 
 @RestController
@@ -20,5 +25,12 @@ public class PostResource {
 	public ResponseEntity<Post> findById(@PathVariable String id) {
 		Post obj = postService.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@GetMapping(value = "/titlesearch")
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+		URL.decodeParam(text);
+		List<Post> list = postService.findByTitle(text);
+		return ResponseEntity.ok().body(list);
 	}
 }

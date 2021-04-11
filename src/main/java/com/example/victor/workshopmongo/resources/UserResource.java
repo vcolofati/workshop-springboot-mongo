8ddc710f93efb654a234d3166e00dee9454c60f4
Victor <vcolofati@gmail.com>
2021-04-11 14:24:06 -0300

    User posts endpoint created

--- src/main/java/com/example/victor/workshopmongo/resources/UserResource.java
+++ src/main/java/com/example/victor/workshopmongo/resources/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.example.victor.workshopmongo.domain.Post;
 import com.example.victor.workshopmongo.domain.User;
 import com.example.victor.workshopmongo.dto.UserDTO;
 import com.example.victor.workshopmongo.services.UserService;
@@ -60,5 +61,11 @@ public class UserResource {
 		obj.setId(id);
 		obj = userService.update(obj);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value = "/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = userService.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 }

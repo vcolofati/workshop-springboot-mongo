7a10b3c15eb65986fd9cc05e698fdd78702b87a5
Victor <vcolofati@gmail.com>
2021-04-11 12:01:55 -0300

    Insert User

--- src/main/java/com/example/victor/workshopmongo/services/UserService.java
+++ src/main/java/com/example/victor/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.victor.workshopmongo.domain.User;
+import com.example.victor.workshopmongo.dto.UserDTO;
 import com.example.victor.workshopmongo.repositories.UserRepository;
 import com.example.victor.workshopmongo.services.exceptions.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = userRepository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return userRepository.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }

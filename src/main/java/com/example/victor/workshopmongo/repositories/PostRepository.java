0282ef456b084d8bbade177ac66a95e75ad27e2c
Victor <vcolofati@gmail.com>
2021-04-11 15:19:10 -0300

    @Query annotation

--- src/main/java/com/example/victor/workshopmongo/repositories/PostRepository.java
+++ src/main/java/com/example/victor/workshopmongo/repositories/PostRepository.java
@@ -3,10 +3,14 @@ package com.example.victor.workshopmongo.repositories;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import com.example.victor.workshopmongo.domain.Post;
 
 public interface PostRepository extends MongoRepository<Post, String> {
 	
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
 }
